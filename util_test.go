a0cfa0b9576b4d72e758e0e07be27bc022c59c35
dinesh <007007dinesh@gmail.com>
2022-11-04 16:34:53 +0530

    Replaced the spf1/viper references

--- util_test.go
+++ util_test.go
@@ -16,7 +16,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/spf13/viper/internal/testutil"
+	"github.com/dineshsivaji/viper/internal/testutil"
 )
 
 func TestCopyAndInsensitiviseMap(t *testing.T) {
